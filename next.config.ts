7ffd38a6d05e02c4ae0960c5a0f89a0cc85a253a
Nitin Bhaskar <nitinbhaskar.2004@gmail.com>
2025-01-05 22:47:06 +0530

    removed linting and /api/mru

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Turn off linting for the entire project
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
